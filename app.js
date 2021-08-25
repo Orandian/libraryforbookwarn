a2781a7154621699fa774417ae7a774b641243e0
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2021-08-25 16:32:06 +0630

    25.8.2021

---
+++ app.js
@@ -0,0 +1,18 @@
+function search(){
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('search');
+    filter = input.value;
+    ul = document.getElementById("result");
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+
+        if (txtValue.indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
