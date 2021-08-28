9df16891245239dba6a04efeb5b12103f96c66dd
Yan Naing Htwe <yannainghtwe.777.yy@gmail.com>
2021-08-28 20:17:57 +0630

    28.8.2021

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function search(){
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById('search');
-    filter = input.value;
+    filter = input.value.toUpperCase();
     ul = document.getElementById("result");
     li = ul.getElementsByTagName('li');
 
@@ -9,7 +9,7 @@ function search(){
         a = li[i].getElementsByTagName("a")[0];
         txtValue = a.textContent || a.innerText;
 
-        if (txtValue.indexOf(filter) > -1) {
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
             li[i].style.display = "";
         } else {
             li[i].style.display = "none";
